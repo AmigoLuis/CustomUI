ffbf71309e79dd5fbea04c882d4e7389874284b9
AmigoLuis <1687339148@qq.com>
2026-01-19 10:57:28 +0800

    1、初始化common ui的学习项目，基于b站视频BV1XiM5zYE1s 2、添加玩家控制类cpp和蓝图 3、创建游戏模式 4、创建测试地图，应用3中的游戏模式 5、测试地图创建地面，应用材质 6、测试地图创建光照（打包的资产） 7、导入项目需要的所有资产

---
+++ Source/CustomUI/Public/PlayerController/FrontEndPlayerController.h
@@ -0,0 +1,17 @@
+// Amigo Luis all rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FrontEndPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMUI_API AFrontEndPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
