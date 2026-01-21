2fe5c626225876d3f48ca0f7ce16805df3deed25
AmigoLuis <1687339148@qq.com>
2026-01-21 11:35:42 +0800

    补充确认界面需要的一些方法、类

--- Source/CustomUI/Private/Widgets/WidgetConfirmation.cpp
+++ Source/CustomUI/Private/Widgets/WidgetConfirmation.cpp
@@ -1,5 +1,73 @@
 // Amigo Luis all rights reserved
 
 #include "Widgets/WidgetConfirmation.h"
+
+#include "UILogger.h"
 #include "Enums/FrontEndEnumTypes.h"
 
+UConfirmWidgetInfoObject* UConfirmWidgetInfoObject::CreateOKConfirmWidget(const FText& Title, const FText& Message, 
+	const EConfirmationWidgetType ConfirmationWidgetType)
+{
+	UConfirmWidgetInfoObject* InfoObject = NewObject<UConfirmWidgetInfoObject>();
+	InfoObject->Title = Title;
+	InfoObject->Message = Message;
+
+	if (ConfirmationWidgetType == EConfirmationWidgetType::OK)
+	{
+		
+		FConfirmWidgetButtonInfo ButtonInfo;
+		ButtonInfo.ButtonText = FText::FromString(TEXT("OK"));
+		ButtonInfo.ConfirmationChoiceType = EConfirmationChoiceType::Closed;
+		InfoObject->ButtonsInfo.Add(ButtonInfo);
+	} else if (ConfirmationWidgetType != EConfirmationWidgetType::Unknown)
+	{
+		FText YesButtonText;
+		FText NoButtonText;
+		FillButtonTextBasedOnEConfirmationWidgetType(
+			YesButtonText,
+			NoButtonText, 
+			ConfirmationWidgetType);
+		
+		FConfirmWidgetButtonInfo YesButtonInfo;
+		YesButtonInfo.ButtonText = YesButtonText;
+		YesButtonInfo.ConfirmationChoiceType = EConfirmationChoiceType::Confirmed;
+		InfoObject->ButtonsInfo.Add(YesButtonInfo);
+		
+		FConfirmWidgetButtonInfo NoButtonInfo;
+		NoButtonInfo.ButtonText = NoButtonText;
+		NoButtonInfo.ConfirmationChoiceType = EConfirmationChoiceType::Canceled;
+		InfoObject->ButtonsInfo.Add(NoButtonInfo);
+	}
+	else
+	{
+		PrintInLog(TEXT("should not create CreateConfirmWidget for Type: ") + 
+			UEnum::GetValueAsString(ConfirmationWidgetType), Error);
+	}
+	return InfoObject;
+}
+
+void UConfirmWidgetInfoObject::FillButtonTextBasedOnEConfirmationWidgetType(
+	FText& InConfirmButtonText,
+	FText& InCancelButtonText,
+	const EConfirmationWidgetType ConfirmationWidgetType)
+{
+	switch (ConfirmationWidgetType)
+	{
+	case EConfirmationWidgetType::YesNo:
+		InConfirmButtonText = FText::FromString(TEXT("Yes"));
+		InCancelButtonText = FText::FromString(TEXT("No"));
+		return;
+	case EConfirmationWidgetType::OkCancel:
+		InConfirmButtonText = FText::FromString(TEXT("OK"));
+		InCancelButtonText = FText::FromString(TEXT("Cancel"));
+		return;
+	case EConfirmationWidgetType::AcceptDecline:
+		InConfirmButtonText = FText::FromString(TEXT("Accept"));
+		InCancelButtonText = FText::FromString(TEXT("Decline"));
+		return;
+	default:
+		PrintInLog(TEXT("can not fill button text for CreateConfirmWidgetType: ") +
+		           UEnum::GetValueAsString(ConfirmationWidgetType), Error);
+		return;
+	}
+}
--- Source/CustomUI/Public/Widgets/WidgetConfirmation.h
+++ Source/CustomUI/Public/Widgets/WidgetConfirmation.h
@@ -3,11 +3,45 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Enums/FrontEndEnumTypes.h"
 #include "Widgets/WidgetActivatableBase.h"
 #include "WidgetConfirmation.generated.h"
 
+enum class EConfirmationChoiceType : uint8;
 class UDynamicEntryBox;
 class UCommonTextBlock;
+
+USTRUCT(BlueprintType)
+struct FConfirmWidgetButtonInfo
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EConfirmationChoiceType ConfirmationChoiceType = EConfirmationChoiceType::Unknown;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText ButtonText;
+};
+
+UCLASS()
+class UConfirmWidgetInfoObject : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(Transient)
+	FText Title;
+	UPROPERTY(Transient)
+	FText Message;
+	UPROPERTY(Transient)
+	TArray<FConfirmWidgetButtonInfo> ButtonsInfo;
+	
+	static UConfirmWidgetInfoObject* CreateOKConfirmWidget(const FText& Title, const FText& Message, 
+		const EConfirmationWidgetType ConfirmationWidgetType);
+	static void FillButtonTextBasedOnEConfirmationWidgetType(FText& InConfirmButtonText, FText& InCancelButtonText,
+		const EConfirmationWidgetType ConfirmationWidgetType);
+};
+
 /**
  * 
  */
