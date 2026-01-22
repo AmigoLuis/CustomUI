f3677e85be4bce0be596732bdc13510ac1c859da
AmigoLuis <1687339148@qq.com>
2026-01-22 10:59:27 +0800

    在设置界面的初始化函数中，绑定后退、点击事件

--- Source/CustomUI/Private/Settings/WidgetSettingsMenu.cpp
+++ Source/CustomUI/Private/Settings/WidgetSettingsMenu.cpp
@@ -3,3 +3,36 @@
 
 #include "Settings/WidgetSettingsMenu.h"
 
+#include "ICommonInputModule.h"
+#include "UILogger.h"
+#include "Widgets/FrontEndButtonBase.h"
+#include "Input/CommonUIInputTypes.h"
+
+void UWidgetSettingsMenu::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	ResetActionHandle = RegisterUIActionBinding(FBindUIActionArgs(
+		ICommonInputModule::GetSettings().GetDefaultBackAction(), true,
+		FSimpleDelegate::CreateUObject(
+			this, 
+			&UWidgetSettingsMenu::OnBackActionTriggeredInSettingsMenu)));
+
+	CHECK_BOOL_TRUE_RETURN_WARN(ResetAction.IsNull());
+	ResetActionHandle = RegisterUIActionBinding(FBindUIActionArgs(
+		ResetAction, true,
+		FSimpleDelegate::CreateUObject(
+			this, 
+			&UWidgetSettingsMenu::OnResetActionTriggeredInSettingsMenu)));
+}
+
+void UWidgetSettingsMenu::OnResetActionTriggeredInSettingsMenu()
+{
+	PrintInLog(TEXT("Reset Action Triggered from Settings Menu"), Display);
+}
+
+void UWidgetSettingsMenu::OnBackActionTriggeredInSettingsMenu()
+{
+	PrintInLog(TEXT("Back Action Triggered from Settings Menu"), Display);
+	DeactivateWidget();
+}
--- Source/CustomUI/Public/Settings/WidgetSettingsMenu.h
+++ Source/CustomUI/Public/Settings/WidgetSettingsMenu.h
@@ -13,5 +13,13 @@ UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
 class CUSTOMUI_API UWidgetSettingsMenu : public UWidgetActivatableBase
 {
 	GENERATED_BODY()
-	
+protected:
+	virtual void NativeOnInitialized() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "FrontEnd Settings Menu", meta = (RowType = "/Script/CommonUI.CommonInputActionDataBase"))
+	FDataTableRowHandle ResetAction;
+	FUIActionBindingHandle ResetActionHandle;
+	void OnResetActionTriggeredInSettingsMenu();
+	void OnBackActionTriggeredInSettingsMenu();
 };
