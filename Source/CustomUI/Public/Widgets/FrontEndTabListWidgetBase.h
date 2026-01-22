b3111207a8f5b6bd75fcf445493e64dcd25036d1
AmigoLuis <1687339148@qq.com>
2026-01-22 12:44:24 +0800

    1、创建设置界面的tab组件，简称tab组件 2、设置tab组件相关属性 3、添加检查函数，检查tab组件属性，并且输出编译时错误日志 4、添加tab组件蓝图

---
+++ Source/CustomUI/Public/Widgets/FrontEndTabListWidgetBase.h
@@ -0,0 +1,29 @@
+// Amigo Luis all rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonTabListWidgetBase.h"
+#include "FrontEndTabListWidgetBase.generated.h"
+
+class UFrontEndButtonBase;
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class CUSTOMUI_API UFrontEndTabListWidgetBase : public UCommonTabListWidgetBase
+{
+	GENERATED_BODY()
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "FrontEnd TabList Settings", 
+		meta=(AllowPrivateAccess="true", ClampMin = "1", ClampMax = "10"))
+	int DebugEditorPreviewTabCount = 3;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "FrontEnd TabList Settings", 
+		meta=(AllowPrivateAccess="true"))
+	TSubclassOf<UFrontEndButtonBase> TabButtonEntryClass;
+
+public:
+#if WITH_EDITOR
+	virtual void ValidateCompiledDefaults(IWidgetCompilerLog& CompileLog) const override;
+#endif
+};
