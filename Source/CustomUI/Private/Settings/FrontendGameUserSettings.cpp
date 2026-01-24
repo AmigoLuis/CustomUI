99896d47f935d3f4f0d4a9ff5fc8781ba469b99d
AmigoLuis <1687339148@qq.com>
2026-01-24 11:27:45 +0800

    创建自定义游戏设置存档类，添加游戏难度设置

---
+++ Source/CustomUI/Private/Settings/FrontendGameUserSettings.cpp
@@ -0,0 +1,20 @@
+// Amigo Luis all rights reserved
+
+
+#include "Settings/FrontendGameUserSettings.h"
+
+#include "UILogger.h"
+
+UFrontendGameUserSettings* UFrontendGameUserSettings::Get()
+{
+	CHECK_NULL_RETURN_VALUE(GEngine, nullptr);
+	UFrontendGameUserSettings* const Settings = Cast<UFrontendGameUserSettings>(GEngine->GetGameUserSettings());
+	if (Settings == nullptr)
+	{
+		PrintInLog(SYMBOL_NAME_TEXT(Cast<UFrontendGameUserSettings>(GEngine->GetGameUserSettings())) 
+			TEXT(" is nullptr, please set ") 
+			SYMBOL_NAME_TEXT(UFrontendGameUserSettings) 
+			TEXT(" as GameUserSetting in project settings."));
+	}
+	return Settings;
+}
---
+++ Source/CustomUI/Public/Settings/FrontendGameUserSettings.h
@@ -0,0 +1,21 @@
+// Amigo Luis all rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "FrontendGameUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMUI_API UFrontendGameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+public:
+	static UFrontendGameUserSettings* Get();
+private:
+	UPROPERTY(Config)
+	FString CurrentGameDifficulty;
+};
