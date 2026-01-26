f47862d5c9521299ccf1b4f826a47cffa95a4f68
AmigoLuis <1687339148@qq.com>
2026-01-26 11:31:37 +0800

    实现清理和设置 setting entry 的 detail信息

--- Source/CustomUI/Private/Settings/WidgetSettingDetailsView.cpp
+++ Source/CustomUI/Private/Settings/WidgetSettingDetailsView.cpp
@@ -3,3 +3,48 @@
 
 #include "Settings/WidgetSettingDetailsView.h"
 
+#include "CommonTextBlock.h"
+#include "CommonLazyImage.h"
+#include "CommonRichTextBlock.h"
+#include "UILogger.h"
+#include "Settings/DataObjects/ListSettingDataObjectBase.h"
+
+void UWidgetSettingDetailsView::UpdateDetailViewInfo(UListSettingDataObjectBase* InDataObject,
+                                                     const FString& InWidgetClass)
+{
+	CHECK_NULL_RETURN(InDataObject);
+	TextBlockTitle->SetText(InDataObject->GetDataDisplayName());
+
+	if (const TSoftObjectPtr<UTexture2D> SoftImage = InDataObject->GetSoftDescriptionImage(); 
+		!SoftImage.IsNull())
+	{
+		LazyImageDescription->SetBrushFromLazyTexture(SoftImage.Get());
+		LazyImageDescription->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+	}
+	RichTextBlockDescription->SetText(InDataObject->GetDescriptionRichText());
+#undef DYNAMIC_DETAILS_TEXT_STYLE
+#define DYNAMIC_DETAILS_TEXT_STYLE TEXT("Bold")
+	const FString& DynamicDetails = 
+		FString::Format(TEXT("Data Object Class: <{0}>{1}</>\n\nEntry Widget Class: <{0}>{2}</>"),{
+			DYNAMIC_DETAILS_TEXT_STYLE,
+			InDataObject->GetClass()->GetName(), 
+			InWidgetClass});
+	RichTextBlockDynamicDetails->SetText(FText::FromString(DynamicDetails));
+#undef DYNAMIC_DETAILS_TEXT_STYLE
+	RichTextBlockDisableReason->SetText(InDataObject->GetDisabledRichText());
+}
+
+void UWidgetSettingDetailsView::ClearDetailViewInfo()
+{
+	TextBlockTitle->SetText(FText::GetEmpty());
+	LazyImageDescription->SetVisibility(ESlateVisibility::Collapsed);
+	RichTextBlockDescription->SetText(FText::GetEmpty());
+	RichTextBlockDynamicDetails->SetText(FText::GetEmpty());
+	RichTextBlockDisableReason->SetText(FText::GetEmpty());
+}
+
+void UWidgetSettingDetailsView::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	ClearDetailViewInfo();
+}
--- Source/CustomUI/Public/Settings/WidgetSettingDetailsView.h
+++ Source/CustomUI/Public/Settings/WidgetSettingDetailsView.h
@@ -6,6 +6,7 @@
 #include "Blueprint/UserWidget.h"
 #include "WidgetSettingDetailsView.generated.h"
 
+class UListSettingDataObjectBase;
 class UCommonTextBlock;
 class UCommonRichTextBlock;
 class UCommonLazyImage;
@@ -16,6 +17,14 @@ UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
 class CUSTOMUI_API UWidgetSettingDetailsView : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	void UpdateDetailViewInfo(UListSettingDataObjectBase* InDataObject, const FString& InWidgetClass = FString());
+	void ClearDetailViewInfo();
+
+protected:
+	// UUserWidget
+	virtual void NativeOnInitialized() override;
+	// UUserWidget
 private:
 	/**** BindWidget ****/
 	UPROPERTY(meta=(BindWidget))
