5ec20eb02007fc3c361b7bb6196458590c3a0d13
AmigoLuis <1687339148@qq.com>
2026-01-28 13:08:28 +0800

    实现判断设置项能否修改的条件逻辑

--- Source/CustomUI/Private/Settings/DataObjects/ListSettingDataObjectBase.cpp
+++ Source/CustomUI/Private/Settings/DataObjects/ListSettingDataObjectBase.cpp
@@ -4,10 +4,41 @@
 #include "Settings/DataObjects/ListSettingDataObjectBase.h"
 
 #include "Settings/FrontendGameUserSettings.h"
+#include "Settings/DataObjects/SettingDataEditConditionDetail.h"
 
 void UListSettingDataObjectBase::InitializeDataObject()
 {
 	OnInitializeDataObject();
+}
+
+void UListSettingDataObjectBase::AddEditCondition(const FSettingDataEditConditionDetail& SettingDataEditConditionDetail)
+{
+	SettingDataEditConditionDetails.Add(SettingDataEditConditionDetail);
+}
+
+bool UListSettingDataObjectBase::IsSettingDataEditable()
+{
+	bool bIsEditable = true;
+	if (SettingDataEditConditionDetails.IsEmpty()) return bIsEditable;
+	
+	FString CachedDisabledRichReason;
+	for (const FSettingDataEditConditionDetail& SettingDataEditCondition : SettingDataEditConditionDetails)
+	{
+		if (!SettingDataEditCondition.IsConditionValid() || SettingDataEditCondition.IsEditable()) continue;
+		bIsEditable = false;
+		CachedDisabledRichReason.Append(SettingDataEditCondition.GetDisabledRichReason());
+		// TODO:确认下这里是否需要移除到循环外
+		SetDisabledRichText(FText::FromString(CachedDisabledRichReason));
+		if (SettingDataEditCondition.HasForcedStringValue())
+		{
+			const FString& ForcedString = SettingDataEditCondition.GetDisabledForcedStringValue();
+			if (CanSetToForcedStringValue(ForcedString))
+			{
+				OnSetToForcedStringValue(ForcedString);
+			}
+		}
+	}
+	return bIsEditable;
 }
 
 void UListSettingDataObjectBase::OnInitializeDataObject()
--- Source/CustomUI/Public/Settings/DataObjects/ListSettingDataObjectBase.h
+++ Source/CustomUI/Public/Settings/DataObjects/ListSettingDataObjectBase.h
@@ -7,6 +7,7 @@
 #include "FunctionLibraries/UtilityMacros.h"
 #include "ListSettingDataObjectBase.generated.h"
 
+struct FSettingDataEditConditionDetail;
 /**
  * 
  */
@@ -35,12 +36,19 @@ public:
 		return TArray<UListSettingDataObjectBase*>();
 	}
 	virtual bool HasChildrenData() const { return false; }
+	// call in setting resigtry to add setting data edit condition
+	void AddEditCondition(const FSettingDataEditConditionDetail& SettingDataEditConditionDetail);
+	bool IsSettingDataEditable();
 	
 	// child class should override it if it supports default value
 	virtual bool HasDefaultValue() const {return false;}
 	virtual bool CanResetToDefaultValue() const {return false;}
 	virtual bool TryResetToDefaultValue() {return false;}
 protected:
+	// child can override this to allow value can be set to forced string value
+	virtual bool CanSetToForcedStringValue(const FString& InForcedValue) const {return false;}
+	// child can override this to specify how value can be set to forced string value
+	virtual void OnSetToForcedStringValue(const FString& InForcedValue) {}
 	// called in InitializeDataObject, children can override it to do custom initialization
 	virtual void OnInitializeDataObject();
 	
@@ -57,4 +65,5 @@ private:
 	UListSettingDataObjectBase* ParentDataRaw;
 	
 	bool bShouldApplySettingChangeImmediately = false;
+	TArray<FSettingDataEditConditionDetail> SettingDataEditConditionDetails;
 };
