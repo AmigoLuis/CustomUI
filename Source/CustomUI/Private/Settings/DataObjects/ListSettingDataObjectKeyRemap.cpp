702000a54209f20b9aff44be4a07a5e98a556076
AmigoLuis <1687339148@qq.com>
2026-01-31 15:31:44 +0800

    1、添加按键重映射数据类型

---
+++ Source/CustomUI/Private/Settings/DataObjects/ListSettingDataObjectKeyRemap.cpp
@@ -0,0 +1,17 @@
+// Amigo Luis all rights reserved
+
+
+#include "Settings/DataObjects/ListSettingDataObjectKeyRemap.h"
+
+#include "UserSettings/EnhancedInputUserSettings.h"
+
+void UListSettingDataObjectKeyRemap::InitKeyRemap(UEnhancedInputUserSettings* InputSettings,
+                                                  UEnhancedPlayerMappableKeyProfile* KeyProfile, ECommonInputType DesiredInputType,
+                                                  const FPlayerKeyMapping& InPlayerKeyMapping)
+{
+	CachedInputSettings = InputSettings;
+	CachedKeyProfile = KeyProfile;
+	CachedDesiredInputType = DesiredInputType;
+	CachedMappingName = InPlayerKeyMapping.GetMappingName();
+	CachedKeySlot = InPlayerKeyMapping.GetSlot();
+}
---
+++ Source/CustomUI/Public/Settings/DataObjects/ListSettingDataObjectKeyRemap.h
@@ -0,0 +1,36 @@
+// Amigo Luis all rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Settings/DataObjects/ListSettingDataObjectBase.h"
+#include "ListSettingDataObjectKeyRemap.generated.h"
+
+struct FPlayerKeyMapping;
+enum class EPlayerMappableKeySlot : uint8;
+enum class ECommonInputType : uint8;
+class UEnhancedPlayerMappableKeyProfile;
+class UEnhancedInputUserSettings;
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMUI_API UListSettingDataObjectKeyRemap : public UListSettingDataObjectBase
+{
+	GENERATED_BODY()
+public:
+	void InitKeyRemap(UEnhancedInputUserSettings* InputSettings, UEnhancedPlayerMappableKeyProfile* KeyProfile, 
+		ECommonInputType DesiredInputType, const FPlayerKeyMapping& InPlayerKeyMapping);
+private:
+	UPROPERTY(Transient)
+	UEnhancedInputUserSettings* CachedInputSettings;
+	
+	UPROPERTY(Transient)
+	UEnhancedPlayerMappableKeyProfile* CachedKeyProfile;
+	
+	ECommonInputType CachedDesiredInputType;
+	
+	FName CachedMappingName;
+	
+	EPlayerMappableKeySlot CachedKeySlot;
+};
